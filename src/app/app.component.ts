066660e9f0b94b1e02da0a7dd76b0e33a21eed13
lukasz <lukasz55szary@gmail.com>
2024-08-13 19:33:27 +0200

    fcv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NotesComponent } from "./components/notes/notes.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NotesComponent, RouterLink ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { NotesComponent } from './components/notes/notes.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: "notes", title: "Notes", component: NotesComponent
+    },
+    // {
+    //     path: "", redirectTo: "/notes", pathMatch: 'full' 
+    // }
+    {
+        path: "**",
+        title: "404 not found",
+        component: NotFoundComponent,
+    },
+    {
+        path: "**",
+        title: "404 not found",
+        component: NotFoundComponent,
+    }
+];
